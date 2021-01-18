f155640c4cc11abd61f044a3673ebdeef8b94514
Aniket <alsulke@mitaoe.ac.in>
2021-01-18 16:08:32 +0530

    new commit

--- Example.java
+++ Example.java
@@ -1,5 +1,5 @@
 class Example {
-	public static void main(String[] args); {
+	public static void main(String[] args) {
 		System.out.println("Helloooo !!");
 		System.out.println("Hello !!");
 		System.out.println("Hello !!");
